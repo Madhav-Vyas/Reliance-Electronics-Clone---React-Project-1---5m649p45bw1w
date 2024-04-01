ff507d922bb3d0930d6ddbdc1775c5d18707c726
Madhav Vyas <madhavvyas2217@gmail.com>
2024-04-01 13:13:15 +0530

    c

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -5,10 +5,10 @@ import axios from 'axios';
 
 const Register = () => {
     const [getData, setData] = useState({
-        name: "test",
-        email: "test6@gmail.com",
-        password: "12345",
-        appType: "ecommerce"
+        name: "",
+        email: "",
+        password: "",
+        appType: ""
     })
     const [getError, setError] = useState(null);
     const navigate = useNavigate();
@@ -38,6 +38,7 @@ const Register = () => {
         }).then((result) => {
             console.log(result);
             navigate('/login');
+            alert("You Have Been SucessFully Registerd Please login");
         }).catch((error) => {
             console.log(error);
             setError("internal server error please try after sometime");
